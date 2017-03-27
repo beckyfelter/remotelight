1f1dee68aa2055b8595738a20aeb6ccc22165b19
beckyfelter <rfelter@vt.edu>
2017-03-27 11:59:05 -0400

    i created my docs folder

---
+++ docs/oxygen-webhelp/search/htmlFileInfoList.js
@@ -0,0 +1,14 @@
+fil = new Array();
+fil["0"]= "c-disclaimer.html@@@Using the remote@@@Legal Disclaimer...";
+fil["1"]= "how-to-program-bulbs-to-groups.html@@@Programming Light Bulbs to Lighting Groups@@@You can program one or more light bulbs to a lighting group to operate that group with your remote control...";
+fil["2"]= "how-to-turn-on-off-dim-lights.html@@@Turning On/Off or Dimming a Lights@@@You can turn on, turn off, or dim a lighting group by pressing the group number button and then pressing the power button or the dimmer control...";
+fil["3"]= "intro-considerations-planning-light-network.html@@@Considerations for Planning Your Lighting Network@@@Before installing your network lighting, you should plan how you want your lighting network to behave...";
+fil["4"]= "intro-low-power-networking.html@@@Low-Power Networking@@@Network lighting operates at a low level of networking power but can successfully connect at long distances because they can send information from light bulb to light bulb...";
+fil["5"]= "intro-max-number-bulbs.html@@@Maximum Number of Network Light Bulbs@@@Testing shows you should network no more than 250 light bulbs to maintain reliable performance...";
+fil["6"]= "intro-network-lighting.html@@@Basic Concepts of Network Lighting@@@You can network LED light bulbs together to operate wirelessly from a remote control...";
+fil["7"]= "intro-product.html@@@Remote Lighting Overview@@@Remote lighting allows you to operate network-based home lighting through a remote control...";
+fil["8"]= "ref-remote-control-components.html@@@Remote Control Components@@@The remote control has components that allow you to program and operate the light bulbs on your lighting network...";
+fil["9"]= "ref-specifications-led-bulb.html@@@LED Light Bulb Specifications@@@Specifications describing the electronics and technology inside the LED light bulb...";
+fil["10"]= "ref-specifications-overview.html@@@Product Specifications@@@The network lighting components have specific electronic and technical specifications...";
+fil["11"]= "ref-specifications-remote-control.html@@@Remote Control Specifications@@@Specifications describing the electronics and technology inside the remote control...";
+fil["12"]= "ts-lightoff.html@@@Light will not turn on@@@The Remote Network Lighting will not turn on Your light bulb is old. The remote&apos;s batteries are dead. Change your light bulb. Change the batteries...";
